277c555e8857bc193eccc7dd46e70db8d496881f
aymen-brahmi <94683894+aymen-brahmi@users.noreply.github.com>
2022-01-09 13:40:27 +0100

    third

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import NavBar from './components/Navbar';
+import Filter from './components/Filter';
+import MovieList from './components/MovieList';
+import Add from './components/Add';
+import {movies} from './components/movies';
+import {useState} from 'react';
+import Details from './components/Details';
+import {Route,Switch} from 'react-router-dom';
+
+//console.log(movies);
+function App() {
+const [movieList,setmovieList]=useState(movies)
+console.log(movieList);
+
+  return (
+
+    <div className="App">
+      <NavBar/>
+      <Filter/>
+      <Add/> 
+      <Switch>
+      
+      <Route exact path="/" render={()=> <MovieList movieList={movieList}  />} />
+      <Route path="/Details/:id" render={ (props) => <Details {...props} list={movies} />   } />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
